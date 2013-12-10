14c2c1bbac2a182c7ec22a18ea4149c9bc972615
crescens <crescens.techstars@gmail.com>
2013-12-10 15:26:51 +0800

    Created methods for the application

--- Camera/Camera/BIDViewController.m
+++ Camera/Camera/BIDViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "BIDViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import <MobileCoreServices/UTCoreTypes.h>
 
 @interface BIDViewController ()
+
+@property (strong, nonatomic) MPMoviePlayerController *moviePlayerController;
+@property (strong, nonatomic) UIImage *image;
+@property (strong, nonatomic) NSURL *movieURL;
+@property (copy, nonatomic) NSString *lastChosenMediaType;
 
 @end
 
@@ -18,12 +25,71 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        self.takePictureButton.hidden = YES;
+    }
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewDidAppear:animated];
+    [self updateDisplay];
+}
+
+- (void)updateDisplay
+{
+    if ([self.lastChosenMediaType isEqual:(NSString *)kUTTypeImage]) {
+        self.imageView.image = self.image;
+        self.imageView.hidden = NO;
+        self.moviePlayerController.view.hidden = YES;
+    } else if ([self.lastChosenMediaType isEqual:(NSString *)kUTTypeMovie]) {
+        [self.moviePlayerController.view removeFromSuperview];
+        self.moviePlayerController = [[MPMoviePlayerController alloc] initWithContentURL:self.movieURL];
+        [self.moviePlayerController play];
+        UIView *movieView = self.moviePlayerController.view;
+        movieView.frame = self.imageView.frame;
+        movieView.clipsToBounds = YES;
+        [self.view addSubview:movieView];
+        self.imageView.hidden = YES;
+    }
+}
+
+- (void)pickMediaFromSource:(UIImagePickerControllerSourceType)sourceType
+{
+    NSArray *mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:sourceType];
+    if ([UIImagePickerController isSourceTypeAvailable:sourceType] && [mediaTypes count] > 0) {
+        NSArray *mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:sourceType];
+        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        picker.mediaTypes = mediaTypes;
+        picker.delegate = self;
+        picker.allowsEditing = YES;
+        picker.sourceType = sourceType;
+        [self presentViewController:picker animated:YES completion:NULL];
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error accessing media" message:@"Device doesn't suport that media source." delegate:nil cancelButtonTitle:@"Draft!" otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+- (UIImage *)shrinkImage:(UIImage *)original toSize:(CGSize)size
+{
+    UIGraphicsBeginImageContextWithOptions(size, YES, 0);
+    [original drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    UIImage *final = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return final;
+}
+
+- (IBAction)shootPictureOrVideo:(id)sender
+{
+    [self pickMediaFromSource:UIImagePickerControllerSourceTypeCamera];
+}
+
+- (IBAction)selectExistingPictureOrVideo:(id)sender
+{
+    [self pickMediaFromSource:UIImagePickerControllerSourceTypePhotoLibrary];
 }
 
 @end
